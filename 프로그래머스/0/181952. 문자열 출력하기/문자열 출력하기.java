7d812d6785ce927425c1a1f063865e9da36fa43a
bonbon0808 <130390467+bonbon0808@users.noreply.github.com>
2024-03-13 15:51:34 +0900

    [level 0] Title: 문자열 출력하기, Time: 176.43 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -3,13 +3,10 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
+        String str = sc.next();
         
-        // 연속된 비교 연산X
-        // 문자열 길이는 length()
-        
-        if(1<= a.length() && a.length() <= 1000000) {
-            System.out.println(a);
+        if(1<= str.length() && str.length() <= 1000000) {
+            System.out.println(str);
         }
     
     }
